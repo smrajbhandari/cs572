f54f06e7050662a324dbe34677a0b573ec79d6ff
smrajbhandari <swagatcatchme@gmail.com>
2019-04-09 10:05:21 -0500

    decrypted the message

--- homework_07/exercise2/routes/secret.js
+++ homework_07/exercise2/routes/secret.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-// var key = 'Need a key';
 
-// const encryptor = require('simple-encryptor')(key);
 
 const MongoClient = require('mongodb').MongoClient;
 const client = new MongoClient('mongodb://homework01:homework01@ds233806.mlab.com:33806/homework01');
@@ -16,7 +14,9 @@ router.get('/', (req, res, next) => {
         const collection = db.collection('data');
         const data = collection.find({}).project({ key: 1, message: 1, _id: 0 }).toArray(function (error, documents) {
             if (err) throw error;
-            res.send(documents);
+            var key = documents[0].key;
+            const encryptor = require('simple-encryptor')(key);
+            res.send(encryptor.decrypt(documents[0].message));
         });
         console.dir("done");
     });
